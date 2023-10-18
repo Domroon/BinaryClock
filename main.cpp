a4be06eda6a41d7cb94e9f638a241a5aa33ae80f
Domroon <haeusser-dominik@web.de>
2023-10-18 16:31:12 +0200

    change char datatype to uint8_t

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -24,36 +25,36 @@ void showStartAnimation(){
     turnOnAll();
     _delay_ms(200);
     turnOffAll();
-    for(int i=0; i<=5; i++){
+    for(uint8_t i=0; i<=5; i++){
         PORTD ^= (1 << i);
         _delay_ms(100);
         PORTD ^= (1 << i);
     }
-    for(int i=0; i<=5; i++){
+    for(uint8_t i=0; i<=5; i++){
         PORTC ^= (1 << i);
         _delay_ms(100);
         PORTC ^= (1 << i);
     }
-    for(int i=0; i<=4; i++){
+    for(uint8_t i=0; i<=4; i++){
         PORTB ^= (1 << i);
         _delay_ms(100);
         PORTB ^= (1 << i);
     }
 }
 
-void initTime(char* hour, char* minute, char* second){
+void initTime(uint8_t* hour, uint8_t* minute, uint8_t* second){
     *hour = 0;
     *minute = 59;
     *second = 58;
 }
 
-void showTime(char* hour, char* minute, char* second){
+void showTime(uint8_t* hour, uint8_t* minute, uint8_t* second){
     PORTB = *hour;
     PORTC = *minute;
     PORTD = *second;
 }
 
-void countSeconds(char* second){
+void countSeconds(uint8_t* second){
     if(*second < 59){
         *second = *second + 1;
     } else {
@@ -61,7 +62,7 @@ void countSeconds(char* second){
     }
 }
 
-void countMinutes(char* minute, char* second){
+void countMinutes(uint8_t* minute, uint8_t* second){
     if(*second == 0){
         if(*minute < 59){
             *minute = *minute + 1;
@@ -71,7 +72,7 @@ void countMinutes(char* minute, char* second){
     }
 }
 
-void countHours(char* hour, char* minute, char* second){
+void countHours(uint8_t* hour, uint8_t* minute, uint8_t* second){
     if(*minute == 0 && *second == 0){
         if(*hour < 23){
             *hour = *hour + 1;
@@ -85,9 +86,9 @@ int main() {
     DDRB = ALL_OUTPUT;          // set whole PORTB as output
     DDRC = ALL_OUTPUT;          // set whole PORTC as output
     DDRD = ALL_OUTPUT;          // set whole PORTD as output
-    char hour;                  // PORTB displays the hours
-    char minute;                // PORTC displays the minutes
-    char second;                // PORTD display the seconds
+    uint8_t hour;                  // PORTB displays the hours
+    uint8_t minute;                // PORTC displays the minutes
+    uint8_t second;                // PORTD display the seconds
     initTime(&hour, &minute, &second);
     showStartAnimation();
 
